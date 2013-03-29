f28faf17b22c4d89933af07f368dfe6bd50ce39e
bighappyface <anomalous20@gmail.com>
2013-03-28 23:41:07 -0500

    Ignore .DS_Store
    Add php5-gd

--- drupalstack/manifests/php.pp
+++ drupalstack/manifests/php.pp
@@ -1,3 +1,3 @@
 class drupalstack::php {
-  package { ['php5', 'php5-mysql', 'libapache2-mod-php5']: ensure => present, }
+  package { ['php5', 'php5-mysql', 'php5-gd', 'libapache2-mod-php5']: ensure => present, }
 }
