a6dcac896fa0f85bc00a3be38ca5c0b881e1d571
bighappyface <anomalous20@gmail.com>
2013-04-05 13:32:54 -0500

    Reduce drupalapp class actions

--- drupalstack/manifests/drupalapp.pp
+++ drupalstack/manifests/drupalapp.pp
@@ -1,20 +1,12 @@
 class drupalstack::drupalapp {
-  file { '/var/www/drupal':
-    ensure  => directory,
-    source  => 'puppet:///modules/drupalstack/drupal',
-    recurse => true,
-    links   => manage,
-    before  => File['/var/www/drupal/sites/default/settings.php'],
-  }
-
   file { '/var/www/drupal/sites/default/settings.php':
-    ensure => present,
-    source => 'file:///var/www/drupal/sites/default/default.settings.php',
+    ensure  => present,
+    mode   => '777',
+    source  => 'file:///var/www/drupal/sites/default/default.settings.php',
   }
 
-  file { '/var/www/drupal/sites/default':
-    mode    => '664',
-    recurse => true,
-    before => File['/var/www/drupal/sites/default/settings.php'],
+  file { '/var/www/drupal/sites/default/files':
+    ensure => directory,
+    mode   => '777',
   }
 }
