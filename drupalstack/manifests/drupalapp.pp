24f80029381ef2ef3ec456561881060f8252e153
bighappyface <anomalous20@gmail.com>
2013-04-09 10:20:12 -0500

    Add copy exec resource to drupalapp class

--- drupalstack/manifests/drupalapp.pp
+++ drupalstack/manifests/drupalapp.pp
@@ -1,12 +1,19 @@
 class drupalstack::drupalapp {
+  exec { 'copy-app':
+    command => '/bin/cp -r /etc/puppet/modules/drupalstack/files/drupal /var/www/drupal',
+    creates => '/var/www/drupal',
+  }
+
   file { '/var/www/drupal/sites/default/settings.php':
     ensure  => present,
-    mode   => '777',
+    mode    => '777',
     source  => 'file:///var/www/drupal/sites/default/default.settings.php',
+    require => Exec['copy-app'],
   }
 
   file { '/var/www/drupal/sites/default/files':
-    ensure => directory,
-    mode   => '777',
+    ensure  => directory,
+    mode    => '777',
+    require => Exec['copy-app'],
   }
 }
